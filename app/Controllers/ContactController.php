13a2abf5060c0a9c878d273f2d5fa8942e1d5c42
David Rodriguez <jdavid.rodriguez.e@gmail.com>
2020-04-22 23:19:59 -0500

    Async tasks

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -18,22 +18,14 @@ Class ContactController extends BaseController
   }
   public function send(ServerRequest $request){
     $requestData = $request->getParsedBody();
+    $message = new Message();
+    $message->name = $requestData['name'];
+    $message->email = $requestData['email'];
+    $message->message = $requestData['message'];
+    $message->sent = false;
+    $message->save();
 
-    $transport = (new Swift_SmtpTransport(getenv('SMTP_HOST'), getenv('SMTP_PORT')))
-      ->setUsername(getenv('SMTP_USER'))
-      ->setPassword(getenv('SMTP_PASS'))
-    ;
-
-    $mailer = new Swift_Mailer($transport);
-
-    $message = (new Swift_Message('Wonderful Subject'))
-      ->setFrom(['contact@contact.com' => 'Contact Person'])
-      ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
-      ->setBody('Hi, you hace a new message. Name' . $requestData['name'] 
-      . 'mail' . $requestData['email'] . 'Message' . $requestData['message'])
-      ;
-
-    $result = $mailer->send($message);
+    
     return new RedirectResponse('/contact');
   }
 }
